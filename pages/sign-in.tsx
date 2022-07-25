e50f3344adc6334960209dc05ac09c1327c3e73b
Parmeet Singh <parmeet.asi@gmail.com>
2022-07-25 20:36:50 +0530

    add sign in flow

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FormLayout } from "../components/layouts";
 import {
   Box,
   Input,
@@ -9,8 +10,6 @@ import {
   PinInput,
   PinInputField,
 } from "@chakra-ui/react";
-import { FileUploadInput } from "../components/form";
-import { FormLayout } from "../components/layouts";
 import { FiArrowRight } from "react-icons/fi";
 
 type Props = {};
@@ -18,28 +17,12 @@ type Props = {};
 const StepOne = ({ goAhead }: { goAhead: () => void }) => {
   return (
     <Box as="form" color="black">
-      <Box
-        pos="absolute"
-        top="2rem"
-        left="50%"
-        transform="translate(-50%, -50%)"
-        bg="white"
-        borderRadius="xl"
-      >
-        <FileUploadInput />
-      </Box>
-      <VStack pt="36" gap="4" w="90%" mx="auto">
-        <VStack alignItems="flex-start" w="100%">
-          <Text as="span" mb="0.05rem" color="black" fontWeight="medium">
-            Name
-          </Text>
-          <Input borderColor="blackAlpha.700" h="61px" />
-        </VStack>
+      <VStack pt="12" gap="4" w="90%" mx="auto">
         <VStack alignItems="flex-start" w="100%">
           <Text as="span" mb="0.05rem" color="black" fontWeight="medium">
             Email
           </Text>
-          <Input borderColor="blackAlpha.700" h="61px" />
+          <Input borderColor="blackAlpha.700" h="61px" type="email" />
         </VStack>
       </VStack>
       <IconButton
@@ -93,19 +76,18 @@ const StepTwo = () => {
   );
 };
 
-const SignUpPage = (props: Props) => {
+const SignInPage = (props: Props) => {
   const [stepCount, setStepCount] = useState(1);
 
   const goAhead = () => {
     setStepCount(sc => sc + 1);
   };
-
   return (
-    <FormLayout heading="Sign Up">
+    <FormLayout heading="Sign In">
       {stepCount === 1 && <StepOne goAhead={goAhead} />}
       {stepCount === 2 && <StepTwo />}
     </FormLayout>
   );
 };
 
-export default SignUpPage;
+export default SignInPage;
