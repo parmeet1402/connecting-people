1e7c46d46993792db3bac2eea874ea310d5293dd
Parmeet Singh <parmeet.asi@gmail.com>
2022-08-09 04:28:20 +0530

    init serverless functions

---
+++ serverless-functions/connectingPeopleAuthChallenge/index.js
@@ -0,0 +1,10 @@
+const { randomDigits } = require("crypto-secure-random-digit");
+
+const mainFunction = () => {};
+
+exports.handler = async event => {
+  return mainFunction();
+};
+
+// TODO: Comment this function call on deployment
+console.log(mainFunction());
